0409b9f27cb01ecdcb338fa6e080e446b6e12a24
richiea1y <44350561+richiea1y@users.noreply.github.com>
2024-10-19 01:01:28 +0800

    add: add class version of Dijkstra's algorithm

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,8 @@ export default defineConfig({
         jsDay1: resolve(__dirname, "js-day-1.html"),
         jsday2: resolve(__dirname, "js-day-2.html"),
         jsday3: resolve(__dirname, "js-day-3.html"),
-        jsday3_dijikstra: resolve(__dirname, "js-day-3-dijikstra.html"),
+        jsday3_dijikstra: resolve(__dirname, "js-day-3-dijikstra-v1.html"),
+        jsday3_dijikstra: resolve(__dirname, "js-day-3-dijikstra-v2.html"),
       },
       output: {
         /* 
